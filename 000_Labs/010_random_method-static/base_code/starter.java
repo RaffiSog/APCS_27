c8e0a93b2ee231adf05ce85459f44e6142771008
RaffiSog <Raffiwas@gmail.com>
2026-09-21 21:38:09 +0000

    update

--- 000_Labs/010_random_method-static/base_code/starter.java
+++ 000_Labs/010_random_method-static/base_code/starter.java
@@ -10,6 +10,14 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		
+		System.out.print("A number between 0 - 9: ");
+		System.out.println((int)(Math.random()*10));
+		System.out.print("A number between 1 - 10: ");
+		System.out.println((int)(Math.random()*10 + 1));
+		System.out.print("A number between 2.5 - 3.5: ");
+		System.out.println((Math.random()+2.5));
+		System.out.print("A double between 14 - 589: ");
+		System.out.println((Math.random()*575+14));
 	}
 }
--- 102_Exercises/10_RandomRange/base_code/starter.java
+++ 102_Exercises/10_RandomRange/base_code/starter.java
@@ -10,6 +10,27 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter 2 numbers to create a range for your random number Please enter an integer: "); 
+		int x = sc.nextInt();
+		System.out.print("Please enter another integer (bigger than the first): ");
+		int y = sc.nextInt();
+		
+		System.out.println();
+		
+		System.out.println("Your range is " + x + " to " + y + ".");
+		System.out.println("Here are 5 numbers generated in that range.");
+		System.out.print((int)(Math.random()*(y-x) + x) + ", ");
+		System.out.print((int)(Math.random()*(y-x) + x) + ", ");
+		System.out.print((int)(Math.random()*(y-x) + x) + ", ");
+		System.out.print((int)(Math.random()*(y-x) + x) + ", ");
+		System.out.println((int)(Math.random()*(y-x) + x) + ", ");
+
+
+
+
+
+
 	}
 }
